af2998258f3e5c84cea1c3ecde215a6eb1bbfbf9
lance <oplancelot@gmail.com>
2023-09-08 14:43:06 +0800

    计算基本正确

--- main.go
+++ main.go
@@ -7,10 +7,19 @@ import (
 
 // Loan represents the loan details.
 type Loan struct {
-	Principal    float64 // 初始本金
-	InterestRate float64 // 年利率
-	TermInMonths int     // 贷款期限（月）
-	StartDate    string  // 放款日期
+	Principal        float64     // 初始本金
+	InterestRate     float64     // 年利率
+	TermInMonths     int         // 贷款期限（月）
+	StartDate        string      // 放款日期
+	RateEntries      []RateEntry // 日期与利率的条目列表
+	RateChangeDate   time.Time   // 年利率变更日期
+	NewInterestRate  float64     // 变更后的年利率
+}
+
+// RateEntry represents the date and interest rate entry.
+type RateEntry struct {
+	Date time.Time // 日期
+	Rate float64   // 利率
 }
 
 // Payment represents the details of each monthly payment.
@@ -26,7 +35,6 @@ type Payment struct {
 
 // CalculateAmortizationSchedule calculates the amortization schedule for the loan.
 func (loan *Loan) CalculateAmortizationSchedule() []Payment {
-	monthlyInterestRate := loan.InterestRate / 12 / 100
 	monthlyPayment := loan.Principal / float64(loan.TermInMonths)
 	payments := make([]Payment, 0)
 
@@ -34,16 +42,16 @@ func (loan *Loan) CalculateAmortizationSchedule() []Payment {
 	totalInterestPaid := 0.0
 
 	// 解析放款日期
-	startDate, _ := parseDate(loan.StartDate)
-	daysInFirstMonth := float64(daysUntilEndOfMonth(startDate))
+	startDate := parseDate(loan.StartDate)
 
 	for month := 1; month <= loan.TermInMonths; month++ {
-		interestPayment := remainingPrincipal * monthlyInterestRate
-		// 第一个月的利息根据天数计算
-		if month == 1 {
-			interestPayment = remainingPrincipal * monthlyInterestRate * (daysInFirstMonth / 31)
-		}
-		// 固定本金支付为每月还款金额
+		// 计算当月的利率
+		currentMonth := startDate.AddDate(0, month, 0)
+
+		
+		currentRate := loan.getInterestRate(currentMonth)
+
+		interestPayment := remainingPrincipal * currentRate / 12 / 100
 		principalPayment := monthlyPayment
 		remainingPrincipal -= principalPayment
 		totalInterestPaid += interestPayment
@@ -55,7 +63,7 @@ func (loan *Loan) CalculateAmortizationSchedule() []Payment {
 			MonthTotalAmount:   monthlyPayment + interestPayment,
 			RemainingPrincipal: remainingPrincipal,
 			TotalInterestPaid:  totalInterestPaid,
-			InterestRate:       loan.InterestRate,
+			InterestRate:       currentRate,
 		}
 		payments = append(payments, payment)
 	}
@@ -63,34 +71,47 @@ func (loan *Loan) CalculateAmortizationSchedule() []Payment {
 	return payments
 }
 
-// 解析日期字符串并返回时间
-func parseDate(dateString string) (time.Time, error) {
-	return time.Parse("20060102", dateString)
+// 获取给定日期的利率
+func (loan *Loan) getInterestRate(date time.Time) float64 {
+	if date.Before(loan.RateChangeDate) || date.Equal(loan.RateChangeDate) {
+		return loan.InterestRate // 在利率变更日期之前使用原始利率
+	}
+	return loan.NewInterestRate // 在利率变更日期之后使用新的利率
 }
 
-// 计算给定日期距离下月的天数
-func daysUntilEndOfMonth(date time.Time) int {
-	year, month, _ := date.Date()
-	nextMonth := time.Date(year, month+1, 18, 0, 0, 0, 0, date.Location())
-	daysUntilEnd := nextMonth.Sub(date).Hours() / 24
-	fmt.Println(daysUntilEnd)
-	return int(daysUntilEnd)
+// 解析日期字符串并返回时间
+func parseDate(dateString string) time.Time {
+    parsedTime, err := time.Parse("2006-01-02", dateString)
+    if err != nil {
+        panic(err)
+    }
+    return parsedTime
 }
+
+
 
 func main() {
 	// 输入贷款信息
 	initialPrincipal := 920000.0 // 初始本金
 	interestRate := 5.05         // 年利率（百分比）
 	loanTerm := 360              // 贷款期限（月）
-	startDate := "20220525"      // 放款日期
+	startDate := "2022-05-25"      // 放款日期
 
-	// 创建 Loan 结构
-	loan := Loan{
-		Principal:    initialPrincipal,
-		InterestRate: interestRate,
-		TermInMonths: loanTerm,
-		StartDate:    startDate,
-	}
+// 创建 Loan 结构
+loan := Loan{
+    Principal:    initialPrincipal,
+    InterestRate: interestRate,
+    TermInMonths: loanTerm,
+    StartDate:    startDate,
+    RateEntries: []RateEntry{
+        {parseDate("2023-06-18"), 4.5}, // 2023年6月18日变更为4.5%的利率
+        {parseDate("2024-06-18"), 4.0}, // 2024年6月18日变更为4.0%的利率
+        // 添加其他日期和利率条目
+    },
+    RateChangeDate:  parseDate("2023-06-18"), // 第一个利率变更日期
+    NewInterestRate: 4.5,                   // 第一个利率变更后的利率
+}
+
 
 	// 计算等额本金还款计划
 	payments := loan.CalculateAmortizationSchedule()
