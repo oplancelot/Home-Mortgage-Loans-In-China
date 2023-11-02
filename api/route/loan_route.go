4e7c0d7e62a7df0399d92ad1144a99da5b393f9f
lance <oplancelot@gmail.com>
2023-11-02 10:08:41 +0800

    get改成post方法,并使用同一个模板提交和显示

--- api/route/loan_route.go
+++ api/route/loan_route.go
@@ -12,16 +12,8 @@ import (
 )
 
 func LoanRoute(env *bootstrap.Env, timeout time.Duration, group *gin.RouterGroup) {
-	// println("api/route/loan_route.go")
-	// group.GET("/loan", func(c *gin.Context) {
-	// 	// c.String(http.StatusOK, "loan")
-	// 	c.HTML(http.StatusOK, "loan.tmpl", gin.H{
-	// 		"title": "Loan",
-	// 	})
-	// })
 
 	group.GET("/loan", func(c *gin.Context) {
-
 		// 解析用户输入的数据
 		principal, _ := strconv.ParseFloat(c.DefaultQuery("principal", "0"), 64)
 		loanTerm, _ := strconv.Atoi(c.DefaultQuery("loanTerm", "12"))
@@ -38,6 +30,41 @@ func LoanRoute(env *bootstrap.Env, timeout time.Duration, group *gin.RouterGroup
 
 		earlyRepayment3Amount, _ := strconv.ParseFloat(c.DefaultQuery("earlyRepayment3Amount", "0"), 64)
 		earlyRepayment3Date := c.DefaultQuery("earlyRepayment3Date", "2099-05-25")
+
+		c.HTML(http.StatusOK, "loan.tmpl", gin.H{
+			"Principal":             principal,             // 从用户输入中获取的初始本金
+			"LoanTerm":              loanTerm,              // 从用户输入中获取的贷款期限
+			"StartDate":             startDate,             // 从用户输入中获取的放款日期
+			"PlusSpread":            plusSpread,            // 从用户输入中获取的上浮点数
+			"PaymentDueDay":         paymentDueDay,         // 从用户输入中获取的还款日
+			"earlyRepayment1Amount": earlyRepayment1Amount, // 从用户输入中获取的提前还款金额
+			"earlyRepayment1Date":   earlyRepayment1Date,   // 从用户输入中获取的提前还款日期
+			"earlyRepayment2Amount": earlyRepayment2Amount, //	从用户输入中获取的提前还款金额
+			"earlyRepayment2Date":   earlyRepayment2Date,   // 从用户输入中获取的提前还款日期
+			"earlyRepayment3Amount": earlyRepayment3Amount, // 从用户输入中获取的提前还款金额
+			"earlyRepayment3Date":   earlyRepayment3Date,   // 从用户输入中获取的提前还款日期
+		})
+
+	})
+
+	group.POST("/loan", func(c *gin.Context) {
+
+		// 解析用户输入的数据
+		principal, _ := strconv.ParseFloat(c.DefaultPostForm("principal", "0"), 64)
+		loanTerm, _ := strconv.Atoi(c.DefaultPostForm("loanTerm", "12"))
+		startDate := c.DefaultPostForm("startDate", "2022-05-25")
+		plusSpread, _ := strconv.ParseFloat(c.DefaultPostForm("plusSpread", "0"), 64)
+		paymentDueDay, _ := strconv.Atoi(c.DefaultPostForm("paymentDueDay", "1"))
+
+		// 获取提前还款信息的值
+		earlyRepayment1Amount, _ := strconv.ParseFloat(c.DefaultPostForm("earlyRepayment1Amount", "0"), 64)
+		earlyRepayment1Date := c.DefaultPostForm("earlyRepayment1Date", "2023-08-19")
+
+		earlyRepayment2Amount, _ := strconv.ParseFloat(c.DefaultPostForm("earlyRepayment2Amount", "0"), 64)
+		earlyRepayment2Date := c.DefaultPostForm("earlyRepayment2Date", "2099-05-25")
+
+		earlyRepayment3Amount, _ := strconv.ParseFloat(c.DefaultPostForm("earlyRepayment3Amount", "0"), 64)
+		earlyRepayment3Date := c.DefaultPostForm("earlyRepayment3Date", "2099-05-25")
 
 		// 创建 Loan 和 EarlyRepayment 的实例
 		originialloan := loan.Loan{
@@ -84,4 +111,5 @@ func LoanRoute(env *bootstrap.Env, timeout time.Duration, group *gin.RouterGroup
 		})
 
 	})
+
 }
